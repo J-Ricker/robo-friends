8af54ee5ce713f764efea9a1760e8387bb6c8cee
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-24 16:14:21 -0400

    Updates

---
+++ precache-manifest.804f998964e108df146ef3784546b071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "89c44f77dfb455c346c6",
+    "url": "/robo-friends/static/css/main.bda5b630.chunk.css"
+  },
+  {
+    "revision": "89c44f77dfb455c346c6",
+    "url": "/robo-friends/static/js/main.89c44f77.chunk.js"
+  },
+  {
+    "revision": "a11b095f7407b1a2b3f5",
+    "url": "/robo-friends/static/css/1.82d36b7c.chunk.css"
+  },
+  {
+    "revision": "a11b095f7407b1a2b3f5",
+    "url": "/robo-friends/static/js/1.a11b095f.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a9a481daa64b3d571204ff19893f0b26",
+    "url": "/robo-friends/index.html"
+  }
+];
